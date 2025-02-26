6fb7ffd6cc90872cf48daa57e079e49676de236a
Milka <141148627+santiago1182@users.noreply.github.com>
2025-02-26 02:33:14 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -114,3 +114,9 @@ function cerrarModal() {
     document.getElementById("modalProducto").style.display = "none";
 }
 
+document.querySelectorAll('.boton-carrito').forEach(boton => {
+    boton.addEventListener('click', (event) => {
+        event.stopPropagation(); // Evita que el clic se propague al contenedor
+        addToCart(boton); // Función que añade al carrito
+    });
+});
